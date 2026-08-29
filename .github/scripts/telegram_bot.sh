b9f0635fda4cc8d00db6da6e4967fd670a595335
C1s44 <mlmy1338@gmail.com>
2026-08-29 15:39:39 +0700

    Update telegram_bot.sh

--- .github/scripts/telegram_bot.sh
+++ .github/scripts/telegram_bot.sh
@@ -9,7 +9,7 @@ $COMMIT_MESSAGE
 [Commit]($COMMIT_URL)
 [Workflow run]($RUN_URL)
 "
-thumbnail="$GITHUB_WORKSPACE/patch/website/1.2.1.jpg"
+thumbnail="$GITHUB_WORKSPACE/patch/website/1.2.1.webp"
 
 file="$1"
 
