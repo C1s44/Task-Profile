b40682a7d1e1764c8bb8df738488364986f70ee0
C1s44 <mlmy1338@gmail.com>
2026-08-29 08:50:16 +0700

    Drop PerfBoost, PerfClamp, OtaProfiles

--- module/service.sh
+++ module/service.sh
@@ -2,4 +2,4 @@
 while [ -z "$(getprop sys.boot_completed)" ]; do
 	sleep 5
 done
-su -lp 2000 -c "cmd notification post -S bigtext -t Task-Profile tag 'Yay! Its time for snacks!😋'"
+su -lp 2000 -c "cmd notification post -S bigtext -t TaskProfile-Utils tag 'Yay! Its time for snacks!😋'"
