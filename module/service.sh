52a783bab460c3e19024cf31d8bba2deb65e96bb
C1s44 <mlmy1338@gmail.com>
2026-08-30 11:05:43 +0700

    Delete module/service.sh

--- module/service.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-while [ -z "$(getprop sys.boot_completed)" ]; do
-	sleep 5
-done
-su -lp 2000 -c "cmd notification post -S bigtext -t Task`Profile tag 'Yay! Its time for snacks!😋'"
