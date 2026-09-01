9f365dced309adea4c39a987e007fd651871de88
C1s44 <mlmy1338@gmail.com>
2026-09-01 17:23:47 +0700

    Update telegram_bot.sh

--- .github/scripts/telegram_bot.sh
+++ .github/scripts/telegram_bot.sh
@@ -1,8 +1,8 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 msg="*$TITLE*
 Branch: $BRANCH
-\\#ci\\_$VERSION
+\#ci_$VERSION
 \`\`\`
 $COMMIT_MESSAGE 
 \`\`\`
@@ -12,8 +12,7 @@ $COMMIT_MESSAGE
 
 file="$1"
 
-
-	response=$(curl -s -F document=@"$file" -F thumbnail=@"$thumbnail" \
+response=$(curl -s -F document=@"$file" \
 	"https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
 	-F "disable_web_page_preview=true" \
