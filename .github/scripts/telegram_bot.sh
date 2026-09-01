2a5a1cc5ba324d70e5d9aac3ea886281f49ed4a1
C1s44 <mlmy1338@gmail.com>
2026-09-01 19:25:50 +0700

    Update telegram_bot.sh

--- .github/scripts/telegram_bot.sh
+++ .github/scripts/telegram_bot.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
 msg="*$TITLE*
 Branch: $BRANCH
-\#ci_$VERSION
+\\#ci\\_$VERSION
 \`\`\`
 $COMMIT_MESSAGE 
 \`\`\`
 [Commit]($COMMIT_URL)
 [Workflow run]($RUN_URL)
 "
+thumbnail="$GITHUB_WORKSPACE/patch/website/webo.jpg"
 
 file="$1"
 
-response=$(curl -s -F document=@"$file" \
+
+	response=$(curl -s -F document=@"$file" -F thumbnail=@"$thumbnail" \
 	"https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
 	-F "disable_web_page_preview=true" \
